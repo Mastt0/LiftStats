2b602b6db9fe72cf5609fa4ff5a2402e2db08f8d
snow <matt01010000@gmail.com>
2023-05-11 18:48:09 -0400

    added radio buttons to workouts

--- App.js
+++ App.js
@@ -1,15 +1,56 @@
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+
+const RadioButton = ({ label, selected, onSelect }) => {
+  return (
+    <TouchableOpacity onPress={onSelect}>
+      <View style={styles.radioButton}>
+        <View
+          style={[
+            styles.radioButtonIcon,
+            { borderColor: selected ? "green" : "gray" },
+          ]}
+        >
+          {selected && <View style={styles.radioButtonSelected} />}
+        </View>
+        <Text style={styles.radioButtonLabel}>{label}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 export default function App() {
+  const [selectedWorkout, setSelectedWorkout] = useState(null);
+
+  const handleWorkoutSelect = (workout) => {
+    setSelectedWorkout(workout);
+  };
+
   return (
     <View style={styles.container}>
       <Text>Welcome to LiftStats!</Text>
       <Text>Saved Workouts:</Text>
-      <Text>Workout 1</Text>
-      <Text>Workout 2</Text>
-      <Text>Workout 3</Text>
-      <Text>Workout 4</Text>
+      <RadioButton
+        label="Workout 1"
+        selected={selectedWorkout === "Workout 1"}
+        onSelect={() => handleWorkoutSelect("Workout 1")}
+      />
+      <RadioButton
+        label="Workout 2"
+        selected={selectedWorkout === "Workout 2"}
+        onSelect={() => handleWorkoutSelect("Workout 2")}
+      />
+      <RadioButton
+        label="Workout 3"
+        selected={selectedWorkout === "Workout 3"}
+        onSelect={() => handleWorkoutSelect("Workout 3")}
+      />
+      <RadioButton
+        label="Workout 4"
+        selected={selectedWorkout === "Workout 4"}
+        onSelect={() => handleWorkoutSelect("Workout 4")}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -21,5 +62,28 @@ const styles = StyleSheet.create({
     backgroundColor: "springgreen",
     alignItems: "center",
     justifyContent: "center",
+  },
+  radioButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 8,
+  },
+  radioButtonIcon: {
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    marginRight: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  radioButtonSelected: {
+    width: 12,
+    height: 12,
+    borderRadius: 6,
+    backgroundColor: "green",
+  },
+  radioButtonLabel: {
+    fontSize: 16,
   },
 });
